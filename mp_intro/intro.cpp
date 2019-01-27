217b4cc71d17839c060390f7470d97b5da05ab47
gk10 <gk10@illinois.edu>
2019-01-27 09:35:26 -0600

    artwork completed

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -1,6 +1,7 @@
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
 #include <math.h>
+#include <cmath>
 
 #include <string>
 
@@ -36,20 +37,35 @@ cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
 
+
+  	//h: 0-360	s: 0-1	l: 0-1 a: 0-1
+
+  	cs225::HSLAPixel *myPixel = new cs225::HSLAPixel(247,0.23,0.5,0.5);
+
   	for(unsigned x = 0; x < 800; x++ ) {
 		for(unsigned y = 0; y < 800; y++ ) {
 
-  		cs225::HSLAPixel & inPixel = png.getPixel(x, y);
-  		inPixel.h = 222;
-  		inPixel.s = 0.5;
-  		inPixel.l = 0.5;
-  		inPixel.a = 0.5;
+		double centerX = 20.0;
+		double centerY = 20.0;
+	   	double distanceX = ((double (centerX)) - x);
+       	double distanceY = ((double (centerY)) - y);
+       	double totalDistance = (std::sqrt ((distanceX * distanceX) + (distanceY * distanceY)));
+
+		cs225::HSLAPixel & inPixel = png.getPixel(x, y);
+  		inPixel = *myPixel;
+
+  		double scaleFactor = 0;
+
+  		scaleFactor = (1 - ((totalDistance * (0.5))/100));
+  		inPixel.h *= scaleFactor; //changes the pixel
+        inPixel.l *= scaleFactor; 
+        inPixel.a *= scaleFactor;
+  		
+  		
   		}
 	}
-  		
 
-
- 
+	
 
   return png;
 }
