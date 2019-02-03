63a9a4022cdcf5e27da3c971b8814b3a7eef5602
gk10 <gk10@illinois.edu>
2019-02-03 12:42:54 -0600

    working on scale functions

--- mp_stickers/Image.cpp
+++ mp_stickers/Image.cpp
@@ -27,25 +27,84 @@ void Image::lighten(double amount){
       			}
 		}
 	}
-	
 }
 void Image::darken(){
-	
+	for (unsigned x = 0; x < this->width(); x++) {
+    	for (unsigned y = 0; y < this->height(); y++) {
+      		HSLAPixel & pixel = this->getPixel(x, y);
+      			if(pixel.l >= 0) {
+      				pixel.l -= 0.1;
+      				if(pixel.l < 0){
+      					pixel.l = 0;
+      				}
+      			}
+		}
+	}
 }
 void Image::darken(double amount){
-	
+	for (unsigned x = 0; x < this->width(); x++) {
+    	for (unsigned y = 0; y < this->height(); y++) {
+      		HSLAPixel & pixel = this->getPixel(x, y);
+      			if(pixel.l >= 0) {
+      				pixel.l -= amount;
+      				if(pixel.l < 0){
+      					pixel.l = 0;
+      				}
+      			}
+		}
+	}
 }
 void Image::saturate(){
-	
+for (unsigned x = 0; x < this->width(); x++) {
+    	for (unsigned y = 0; y < this->height(); y++) {
+      		HSLAPixel & pixel = this->getPixel(x, y);
+      			if(pixel.s <= 1) {
+      				pixel.s += 0.1;
+      				if(pixel.s > 1){
+      					pixel.s = 1;
+      				}
+      			}
+		}
+	}	
 }
 void Image::saturate(double amount){
-	
+	for (unsigned x = 0; x < this->width(); x++) {
+    	for (unsigned y = 0; y < this->height(); y++) {
+      		HSLAPixel & pixel = this->getPixel(x, y);
+      			if(pixel.s <= 1) {
+      				pixel.s += amount;
+      				if(pixel.s > 1){
+      					pixel.s = 1;
+      				}
+      			}
+		}
+	}
 }
 void Image::desaturate(){
-	
+for (unsigned x = 0; x < this->width(); x++) {
+    	for (unsigned y = 0; y < this->height(); y++) {
+      		HSLAPixel & pixel = this->getPixel(x, y);
+      			if(pixel.s >= 0) {
+      				pixel.s -= 0.1;
+      				if(pixel.s < 0){
+      					pixel.s = 0;
+      				}
+      			}
+		}
+	}	
 }
 void Image::desaturate(double amount){
-	
+	for (unsigned x = 0; x < this->width(); x++) {
+    	for (unsigned y = 0; y < this->height(); y++) {
+      		HSLAPixel & pixel = this->getPixel(x, y);
+      			if(pixel.s >= 0) {
+      				pixel.s -= amount;
+      				if(pixel.s < 0){
+      					pixel.s = 0;
+      				}
+      			}
+		}
+	}
 }
 void Image::grayscale(){
 	for (unsigned x = 0; x < this->width(); x++) {
@@ -59,7 +118,29 @@ void Image::rotateColor(double degrees){
 	
 }
 void Image::illinify(){
-	
+	double orange = 11.0;
+  	double blue = 216.0;
+
+  for (unsigned x = 0; x < this->width(); x++) {
+    for (unsigned y = 0; y < this->height(); y++) {
+      
+      HSLAPixel & pixel = this->getPixel(x, y);
+      
+    
+        if((std::abs (pixel.h - orange)) <= (std::abs (pixel.h - blue))){ 
+          pixel.h = 11.0;
+        } 
+        else if((371 - pixel.h) <= (std::abs (pixel.h - blue))){
+          pixel.h = 11.0;
+        }
+        else {
+          pixel.h = 216.0;
+        }
+      }
+  }
+  
+  //orange 11
+  //blue 216
 }
 void Image::scale(double factor){
 	
--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -9,13 +9,18 @@ int main() {
 
 	//cs225::PNG png, png2, result;
 	Image firstPic;
+	PNG secondPic(450,300);
 
-	cout << "width: " << firstPic.width() << " height: " << firstPic.height() << endl;
+	//cout << "width: " << firstPic.width() << " height: " << firstPic.height() << endl;
+	cout << "width: " << secondPic.width() << " height: " << secondPic.height() << endl;
 
   	firstPic.readFromFile("alma.png");
-  	cout << "width: " << firstPic.width() << " height: " << firstPic.height() << endl;
-  	firstPic.lighten(0.3);
+  	
+  	//cout << "width: " << firstPic.width() << " height: " << firstPic.height() << endl;
+  	firstPic.illinify();
+
   	firstPic.writeToFile("myImage.png");
+  	secondPic.writeToFile("otherImage.png");
 
   //
   // Reminder:
