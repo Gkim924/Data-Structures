3a49f41fe95d8542210a85a5a3ae22c20a5b21c5
gk10 <gk10@illinois.edu>
2019-04-05 10:11:09 -0500

    lab finished

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,21 @@
 /* Your code here! */
+#include "dsets.h"
+
+
+void addelements(int num){
+	for(){
+		
+	}
+}
+
+int find(int elem){
+
+}
+
+void setunion(int a, int b){
+
+}
+
+int size(int elem){
+
+}
--- mp_mazes/dsets.h
+++ mp_mazes/dsets.h
@@ -1,1 +1,25 @@
 /* Your code here! */
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class DisjointSets {
+
+public:
+	void addelements(int num);
+
+	int find(int elem);
+
+	void setunion(int a, int b);
+
+	int size(int elem);
+
+private:
+
+	vector<int> table_;
+
+}
+
